894b3663240edfb5f9ac6ab5991513ab7125c188
Ramesh Kumar <codeofnode@gmail.com>
2017-04-30 00:21:33 +0530

    assign

--- common_methods/assign/index.js
+++ common_methods/assign/index.js
@@ -5,7 +5,7 @@ module.exports = function(require, GLOBAL_APP_CONFIG,GLOBAL_METHODS){
     if(typeof bb === 'object' && bb){
       var kys = Object.keys(bb), kl = kys.length;
       for(var j =0; j< kl; j++){
-        if(!noob || (typeof ab[kys[j]] !== 'object')|| (typeof bb[kys[j]] !== 'object')){
+        if(noob !== true || (typeof ab[kys[j]] !== 'object')|| (typeof bb[kys[j]] !== 'object')){
           ab[kys[j]] = bb[kys[j]];
         }
       }
@@ -13,5 +13,16 @@ module.exports = function(require, GLOBAL_APP_CONFIG,GLOBAL_METHODS){
     return ab;
   }
 
-  return func;
+  function main(){
+    var ln = arguments.length, noob = arguments[ln-1];
+    if(noob === true){ ln--; }
+    else noob = false;
+    var no = func(arguments[0],arguments[1]);
+    for(var j =2;j<ln;j++){
+      func(no,arguments[j],noob);
+    }
+    return no;
+  }
+
+  return main;
 }
